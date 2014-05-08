44baa3b17c9a94ecd03c1d665599347809c8815b
Karl Mutch <karl.mutch@itron.com>
2014-05-08 11:04:42 -0700

    Encountered FizzBuzz for the first time outside of web sites etc so will add it.

---
+++ src/com/karlmutch/FizzBuzz.java
@@ -0,0 +1,45 @@
+/**
+ * FizzBuzz, while I heard rumors people do ask about it I had never encountered the 
+ * occasion until recently so here it is.
+ * 
+ * This is a simple problem but one that requires some analysis to realize that there
+ * is an overlap, common factors in the case of FizzBuzz, between conditions that the 
+ * interviewee needs to both detect and address.
+ *
+ */
+
+package com.karlmutch;
+
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
+public class FizzBuzz {
+	
+	public static ArrayList<String> DoFizzBuzz(int startingNumber, int endingNumber)
+	{
+		ArrayList<String> results = new ArrayList<String>();
+		
+		IntStream.rangeClosed(startingNumber, endingNumber).forEach(aNumber -> 
+		{
+			if (aNumber % (5 * 3) == 0) {
+				results.add("FizzBuzz");
+			}
+			else {
+				if (aNumber % 3 == 0) {
+					results.add("Fizz");					
+				}
+				else {
+					if (aNumber % 5 == 0) {
+						results.add("Buzz");					
+					}
+					else {
+						results.add(Integer.toString(aNumber));
+					}
+				}
+			}
+		});
+
+		return(results);
+	}
+
+}
