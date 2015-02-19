a229683756fc18e458862831c99706e320842203
Karl Mutch <karl.mutch@itron.com>
2015-02-19 14:24:44 -0800

    Very simplistic calculator example without precedence just using simple expression ordering etc with simple unit test done

--- src/test/java/com/karlmutch/steps/SimpleCalculatorTestSteps.java
+++ src/test/java/com/karlmutch/steps/SimpleCalculatorTestSteps.java
@@ -1,5 +1,38 @@
+/**
+ *
+ * (c) 2015 by Karl Mutch is licensed under a Creative Commons Attribution 4.0 International License.
+ * 
+ */
 package com.karlmutch.steps;
 
-public class SimpleCalculatorTestSteps {
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
+import com.karlmutch.RunParameters;
+import com.karlmutch.SimpleCalculator;
+
+public class SimpleCalculatorTestSteps 
+{
+	private final RunParameters mParameters;
+	
+	private Float mResult;
+	
+	public SimpleCalculatorTestSteps(RunParameters parameters)
+	{
+		mParameters = parameters;
+	}
+
+	@When("^performing a trivial calculation$")
+	public void select()
+	{
+		mResult = SimpleCalculator.performSimpleCalculation(mParameters.mString.get());
+	}
+	
+	@Then("^the result will be (\\d+)$")
+	public void check(float expectedResult)
+	{
+    	assertThat(mResult, is(expectedResult));
+	}
 }
